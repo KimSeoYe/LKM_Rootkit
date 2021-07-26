1f14965ef24cabac346fcadb20dab7e7dc5d088d
KimSeoYe <kimseoye15@gmail.com>
2021-07-26 14:29:41 +0900

    Add bingo.c
    It calls system() to execute echo command
    It gets a target pid as an argument

--- bingo.c
+++ bingo.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int
 main (int argc, char * argv[]) 
 {
-	FILE * fp ;
+	if (argc != 2) {
+		perror("./bingo <target_pid>\n") ;
+		exit(1) ;
+	}
+
+	char * target_pid =argv[1] ;
 	
-	
+	//printf(">> ") ;
+	//scanf("%d", &target_pid) ;
+
+	char command[128] ;
+	sprintf(command, "echo \"%s\" > /proc/dogdoor", target_pid) ;
+
+	int ret = system(command) ;
+	if (ret == -1 || ret == 127) {
+		perror("system") ;
+		exit(1) ;
+	}
 }
+
